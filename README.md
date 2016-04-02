e51dd0ab5c33da9975b89d8b9cd2cdc4276ad415
Chris Casola <chriscasola@gmail.com>
2016-04-02 16:53:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# atom-jump-to-spec package
+
+This package adds a command to jump to the spec file corresponding to the source file that is currently active.
---
+++ spec/atom-jump-to-spec-spec.js
@@ -0,0 +1,50 @@
+'use babel';
+
+import fs from 'fs-plus';
+import temp from 'temp';
+import wrench from 'wrench';
+import path from 'path';
+
+describe('AtomJumpToSpec', () => {
+  let workspaceElement, rootDir, fixturesPath, activatePromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activatePromise = atom.packages.activatePackage('atom-jump-to-spec');
+
+    waitsForPromise(() => {
+      return activatePromise;
+    });
+
+    atom.commands.dispatch(workspaceElement, 'atom-jump-to-spec:toggle');
+
+    rootDir = fs.realpathSync(temp.mkdirSync('root-dir'));
+    fixturesPath = atom.project.getPaths()[0];
+
+    wrench.copyDirSyncRecursive(
+      path.join(fixturesPath, 'root-dir'),
+      rootDir,
+      { forceDelete: true }
+    );
+
+    atom.project.setPaths([rootDir]);
+
+    waitsForPromise(() => {
+      return atom.workspace.open(path.join(rootDir, 'src/sample.js'));
+    });
+  });
+
+  fdescribe('open spec file', () => {
+    it('opens the test file corresponding to the current open file', () => {
+      atom.commands.dispatch(workspaceElement, 'atom-jump-to-spec:open-spec');
+
+      waitsFor(() => {
+        return atom.workspace.getActiveTextEditor() && atom.workspace.getActiveTextEditor().getPath().indexOf('sample-spec') > -1;
+      });
+
+      runs(() => {
+        expect(atom.workspace.getActiveTextEditor().getPath()).toBe(path.join(rootDir, 'spec/sample-spec.js'));
+      });
+    });
+  });
+});
